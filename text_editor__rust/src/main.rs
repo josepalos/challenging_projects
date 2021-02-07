5d245f7ca452bb723a74f2725a1c7c9255badb46
Josep <josep.alos.pascual@hotmail.com>
2021-02-07 13:33:16 +0100

    refactor(text_editor): extract logic to parse CLI args

--- text_editor__rust/src/main.rs
+++ text_editor__rust/src/main.rs
@@ -3,12 +3,27 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
 
-    let contents = fs::read_to_string(filename)
+    let config = Config::new(&args);
+
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     for (i, line) in contents.split("\n").enumerate() {
         println!("{} | {}", i, line);
     }
 }
+
+struct Config {
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // Clone as Config cannot take ownership of the strings
+        let filename = args[1].clone();
+
+        Config { filename }
+    }
+}
+
