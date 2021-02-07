303c17dde24b63b95acca239eaeb41b402ec70b5
Josep <josep.alos.pascual@hotmail.com>
2021-02-07 13:39:04 +0100

    feat(text_editor): manage errors when parsing CLI args

--- text_editor__rust/src/main.rs
+++ text_editor__rust/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
@@ -19,11 +23,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
         // Clone as Config cannot take ownership of the strings
         let filename = args[1].clone();
 
-        Config { filename }
+        Ok(Config { filename })
     }
 }
 
